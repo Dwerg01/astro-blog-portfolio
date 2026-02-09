52157a8b414a45b2088608a52cefb536c2629d53
Dwerg01 <nardum@gmail.com>
2026-02-09 09:42:27 +0200

    first mods completed and home page starting to take shape

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const HOME_TITLE = 'Shopify Developer | Conversion-Focused Shopify Expert | Nardu';
+export const HOME_DESCRIPTION = 'Professional Shopify developer creating high-converting, brand-aligned stores. Boost your sales with custom Shopify design and CRO. Get in touch.';
+export const ABOUT_TITLE = 'About Me | From No Code, to Shopify Builder';
+export const ABOUT_DESCRIPTION = 'Shopify developer in Johannesburg helping merchants build secure, scalable, and profitable Shopify stores that grow sales.';
